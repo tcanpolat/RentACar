47cfc8da6723640e3dc6007cef24628db22b62b5
tcanpolat <thsn_cnplt@windowslive.com>
2025-02-09 10:45:35 +0300

    initial project

---
+++ RentACar/ORM/Context/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using RentACar.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.ORM.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            Database.Connection.ConnectionString = "Server=.;Database=RentACar;uid=sa;password=1";
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Arabalar> Arabalars { get; set; }
+        public DbSet<Kiralama> Kiralamas { get; set; }
+        public DbSet<Yoneticiler> Yoneticilers { get; set; }
+    }
+}
---
+++ RentACar/ORM/Entity/Kiralama.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.ORM.Entity
+{
+    public class Kiralama : BaseEntity
+    {
+        public int ArabalarID { get; set; }
+        public int YoneticilerID { get; set; }
+        public DateTime AlisTarihi { get; set; }
+        public DateTime TeslimTarihi { get; set; }
+        public DateTime? KiralamaSuresi { get; set; }
+        public double FaturaTutari { get; set; }
+    }
+}
