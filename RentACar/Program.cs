752e0d9d9cb85a9acca1c4f7c9131c94313c5708
tcanpolat <95044528+tcanpolat@users.noreply.github.com>
2025-02-09 10:47:50 +0300

    Update Program.cs

--- RentACar/Program.cs
+++ RentACar/Program.cs
@@ -8,6 +8,7 @@ namespace RentACar
 {
     internal static class Program
     {
+        // Tahsin
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
