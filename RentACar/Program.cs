e46fd8e3065b7c9fee18f3cd774de6b62f8be51f
tcanpolat <thsn_cnplt@windowslive.com>
2025-03-02 11:53:33 +0300

    Update Program.cs

--- RentACar/Program.cs
+++ RentACar/Program.cs
@@ -14,6 +14,7 @@ namespace RentACar
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmKullaniciGiris());
